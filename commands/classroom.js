f185a8bda9be7a3a5e96726ac552d18f03091c3c
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-11-21 17:38:02 +0800

    Classroom

--- commands/classroom.js
+++ commands/classroom.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const { dev } = require("../json/roles.json");
+
 module.exports = {
     name: "classroom",
     aliases: ["class"],
@@ -15,21 +16,6 @@ module.exports = {
      * @param   {Array<String>}       args 
      */
     async execute(message, args) {
-        const { Gclass } = require("../classes/gclass");
-        try {
-            //await Gclass.authorize();
-    
-            const English = new Gclass(155665494667);
-            const ann = await English.getAnnouncements(3);
-            const ah = [];
-            for (const Aclass of ann) {
-                ah.push(Aclass.text);
-            }
-            //console.log(ah);
-            const charCounter = (str, max = 1024) => str.length > max ? `${str.slice(0, max - 3)}...` : str;
-            message.channel.send(charCounter(ah.join("\n\n")));       
-        } catch (error) {
-            console.error(error);
-        }
+        // Create command hanler for class commands
     }
 };
