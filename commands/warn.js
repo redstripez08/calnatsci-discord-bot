59a3bc1b81eccffe0f11b1b42b67f6116fd00686
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-11-30 22:33:11 +0800

    Modified warn. WIP

--- commands/warn.js
+++ commands/warn.js
@@ -1,3 +1,6 @@
+const Discord = require("discord.js");
+const { dev } = require("../json/roles.json");
+
 module.exports = {
     name: "warn",
     aliases: null,
@@ -6,28 +9,89 @@ module.exports = {
     cooldown: 0,
     guildOnly: true,
     args: true,
-    roles: [],
+    roles: [dev],
+    /**
+     * Does warning stuff
+     * @param   {Discord.Message}       message 
+     * @param   {Array<String>}         args 
+     */
     async execute(message, args) {
-        // if (!message.member.roles.cache.has("772811239604224023")) {
-        //     return message.channel.send("Only memebers with `Dev` role can use this.\nReason: WIP")
-        // }
-
-        if (message.author.id !== "627065133599817738") {
-            return message.channel.send("Only memebers with `Dev` role can use this.\nReason: WIP")
-        }
-
-        const fs = require("fs");
-        const huh = JSON.stringify(
-            {
-                warn: "My pen"
-            }
-        );
         try {
-            const data = await fs.promises.readFile("./json/warn.json");
-            console.log(JSON.parse(data));
+            const mongoose = require("mongoose");
+            args.shift();
 
-            await fs.promises.writeFile("./json/warn.json", huh);
+            const mentioned = message.mentions.users.first();
+            if (!message.mentions.users.size) return message.channel.send("You need to mention someone!");
+            if (!args.length) return message.channel.send("You need to supply a warning!");
+
+            const WarnSchema = mongoose.Schema({
+                name: {
+                    type: String,
+                    required: true
+                },
+
+                warning: {
+                    type: String,
+                    required: true
+                },
+
+                issued_by: {
+                    type: String,
+                    required: true
+                },
+
+                date: {
+                    type: Date,
+                    default: Date.now
+                }
+            });
+
+            const MyModel = mongoose.model("warnings", WarnSchema);
+            const instance = new MyModel({
+                name: mentioned.id,
+                warning: args.join(" "),
+                issued_by: message.author.id,
+            });
+
+            const flagRegex = new RegExp(/-\w+|--\w{2,}/gi);
+            const flagArr = args.filter(flag => flagRegex.test(flag));
+            console.log(flagArr);
+            switch (true) {
+                case flagArr.includes("-l") || flagArr.includes("--logs"):
+                    logs();
+                    break;
+            
+                default:
+                    break;
+            }
+
+
+
+            // const lol = await instance.save();
+            // console.log(lol);
+            async function logs(params) {
+                const je = await MyModel.find({name: mentioned.id});
+                console.log(je);
+                const accused = message.client.users.cache.get(je[0].name);
+                const accuser = message.client.users.cache.get(je[0].issued_by);
+
+                message.channel.send(`Accused: ${accused}\nAccuser: ${accuser}\nWarning: ${je[0].warning}\nDate: ${je[0].date}`);
+                return;
+            }
+
+            return;
+
+            message.channel.send("Warning Sent and Stored.");
+
+            const warnEmbed = new Discord.MessageEmbed()
+                .setTitle(`You have been warned by ${message.author.tag}\n`)
+                .setColor("#FF0000")
+                .setDescription(args.join(" "))
+                .setFooter("All warnings are stored.")
+
+            mentioned.send(warnEmbed).catch(() => {});
         } catch (error) {
+            message.channel.send(`There was an error:\n\`${error}\``);
             console.error(error);
         }
     }
