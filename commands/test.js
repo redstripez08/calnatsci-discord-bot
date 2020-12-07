2e41b4b10bd00cd4c29210f72fa7c5c8a07beefd
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-12-07 23:49:42 +0800

    Test

--- commands/test.js
+++ commands/test.js
@@ -1,11 +1,30 @@
 module.exports = {
     name: "test",
     aliases: ["t"],
-    execute(message, args) {
-        if (!args.length) {
-            message.channel.send("BIGG CHUNGUS");
-        } else {
-            message.channel.send(args.join(" "));
+    async execute(message, args) {
+        const Discord = require("discord.js");
+        const path = require("path");
+        const fs = require("fs");
+
+        const fetch = require("node-fetch");
+        
+        try {
+            if (!args.length) {
+                // message.channel.send("BIGG CHUNGUS");
+                return message.channel.send("TESTT");
+
+                const res = await fetch("", {method: 'GET'});
+                // console.log(res.body);
+                const heh = await res.buffer();
+
+                const att = new Discord.MessageAttachment(heh);
+                message.channel.send(att);
+            } else {
+                message.channel.send(args.join(" "));
+            }
+        } catch (error) {
+            message.channel.send("There was an error:\n" + `\`${error}\``);
+            console.error(error);
         }
     }
 };
