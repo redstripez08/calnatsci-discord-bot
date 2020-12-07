f89e605640a228f2f6ba61a24fc98398db19b84b
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-12-07 23:42:19 +0800

    Added some routing and file serving

--- server/server.js
+++ server/server.js
@@ -1,21 +1,47 @@
 const http = require("http");
-const fs = require("fs");
+const fs = require("fs").promises;
+const path = require("path");
 
-function server(port) {
+async function serve(port) {
+    try {
+        const html = await fs.readFile(path.join(__dirname, "../public/index.html"));
+        const style = await fs.readFile(path.join(__dirname, "../public/css/style.css"));
+        const js = await fs.readFile(path.join(__dirname, "../public/js/main.js"));
+
+        const server = http.createServer((req, res) => {
+
+            switch (req.url) {
+                case "/css/style.css":
+                    res.setHeader("Content-Type", "text/css");
+                    res.writeHead(200);
+                    res.write(style);
+                    break;
+
+                case "/js/main.js":
+                    res.setHeader("Content-Type", "application/javascript");
+                    res.writeHead(200);
+                    res.write(js);
+                    break;
+            
+                // case "/assets/favicon.ico":
+                //     res.setHeader("Content-Type", "image/x-icon");
+                //     res.writeHead(200);
+                //     require("fs").createReadStream(path.join(__dirname, "../public/assets/favicon.ico")).pipe(res);
+                
+                default:
+                    res.setHeader("Content-Type", "text/html");
+                    res.writeHead(200);
+                    res.write(html);
+                    break;
+            }
     
-    const server = http.createServer((req, res) => {
-        fs.readFile("./website/index.html", "UTF-8", (err, data) => {
-            if (err) throw new Error(err);
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(data);
+            res.end();
         });
-    });
     
-    server.listen(port);
+        server.listen(port, err => err ? (e => {throw new Error(e)})(err) : console.log(`Listening to Port ${process.env.PORT}`));
+    } catch (error) {
+        throw new Error(error);
+    }
 }
 
-server(process.env.PORT);
-console.log("Listening to Port " + process.env.PORT);
-
-//module.exports = server;
+serve(process.env.PORT);
