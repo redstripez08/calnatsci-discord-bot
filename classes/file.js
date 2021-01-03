7ed47a5d22d2ceb40620785b06443b9537901331
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-01-03 17:45:11 +0800

    File Utility to make file handling easier

--- classes/file.js
+++ classes/file.js
@@ -1,43 +1,100 @@
 const fs = require("fs");
+const path = require("path");
 
-/** Represents a file instance, but promisified. */
+/**
+ * @typedef EncodingOptions
+ * @type {"ascii"|"base64"|"binary"|"hex"|"latin1"|"ucs-2"|"ucs2"|"utf-8"|"utf16le"|"utf8"}
+ */
+
+ /**
+  * @callback FileCallback
+  * @param  {Error}             err     Error
+  * @param  {String|Buffer}     data    Data
+  */
+
+/**
+ * @typedef FileOptions    Options for the File class
+ * @type {Object}
+ * 
+ * @property    {EncodingOptions}   [encoding="utf-8"]      Encoding of the file. Defaults to UTF8
+ * @property    {String}            [flags]                 Accepted flags        
+ */
+
+/** Represents a file instance. Supports promises and callbacks */
 class File {
     /**
      * Creates a file instance, using promises instead of callbacks.
-     * @param   {String}        path        Directory of file.
+     * @param   {!String}       path              Directory of the file.
+     * @param   {FileOptions}   [fileOptions]     Options for the file.
      */
-    constructor(path) {
+    constructor(path, fileOptions) {
+        if (!path || typeof path !== "string") return console.error(new Error("Path Required!"));
+        if (!fileOptions || !fileOptions.encoding) fileOptions.encoding = "utf-8";
+
         this.path = path;
+        this.fileOptions = fileOptions;
     }
 
     /**
      * Writes into file.
-     * @param   {String}        content     Content to write into file
+     * @param   {String|Buffer}     content                                 Content to write into file
+     * @param   {String}            [encoding=this.fileOptions.encoding]    Encoding of the content
+     * @param   {FileCallback}      callback                                Callback
      * @returns {Promise<void>}             Void Promise
      */
-    async write(content) {
+    async writeFile(content, encoding = this.fileOptions.encoding, callback) {
         try {
-            const lol = await fs.promises.writeFile(this.path, content);
-            return Promise.resolve(lol);
+            const data = await fs.promises.writeFile(this.path, content, {encoding});
+            if (callback && typeof callback === "function") return callback(null, data);
+            return Promise.resolve(data);
+        } catch (error) {
+            if (callback && typeof callback === "function") return callback(error);
+            return Promise.reject(error);
+        }
+    }
+
+    /**
+     * Writes into file synchronously.
+     * @param   {String|Buffer} content     Content to write into file
+     * @param   {String}        encoding    Encoding of the content
+     * @returns {Void}
+     */
+    writeFileSync(content, encoding) {
+        fs.writeFileSync(this.path, content, {encoding});
+    }
+
+    /**
+     * Asynchronously reads the entire contents of a file.
+     * @param   {EncodingOptions}                   [encoding=this.fileOptions.encoding]    Returned encoding of File. `UTF-8` by default.
+     * @returns {Promise<String>|Promise<Object>}                                           Promise of a String or Object
+     */
+    async readFile(encoding = this.fileOptions.encoding) {
+        try {
+            const data = await fs.promises.readFile(this.path, {encoding});
+            return Promise.resolve(data);
         } catch (error) {
             return Promise.reject(error);
         }
     }
 
     /**
-     * Reads the file and returns its content.
-     * @param   {Boolean}                           json        True by Default. If true, returns Object. Otherwise it returns String.
-     * @param   {String}                            encoding    Returned encoding of File. `UTF-8` by default.
-     * @returns {Promise<String>|Promise<Object>}               Promise of a String or Object
+     * Reads the file and returns its content synchronously
+     * @param {EncodingOptions}                     encoding 
      */
-    async read(json = true, encoding = "utf-8") {
+    readFileSync(encoding = this.fileOptions.encoding) {
         try {
-            const data = await fs.promises.readFile(this.path, encoding);
-            if (json) return Promise.resolve(JSON.parse(data));
-            else return Promise.resolve(data);
+            return fs.readFileSync(this.path, {encoding});
         } catch (error) {
-            return Promise.reject(error);
+            return new Error(error);
         }
+    }
+
+    /**
+     * Returns a new `ReadStream` object.
+     * @returns {fs.ReadStream}
+     */
+    createReadStream() {
+        return new fs.ReadStream(this.path, {encoding});
     }
 
 }
