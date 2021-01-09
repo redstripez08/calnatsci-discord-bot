95cfe7168c0c3a21bcf777b45a91d21f1f8b3505
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-01-10 02:15:46 +0800

    Renamed variables

--- ready_commands/bibleverse.js
+++ ready_commands/bibleverse.js
@@ -6,10 +6,10 @@ const { BIBLE_WEBHOOK_ID, BIBLE_WEBHOOK_TOKEN } = process.env;
 const { File } = require("../classes");
 
 const webhook = new Discord.WebhookClient(BIBLE_WEBHOOK_ID, BIBLE_WEBHOOK_TOKEN);
-const verseFile = new File(path.resolve(__dirname, "../config/verses.toml"), {encoding: "utf-8"});
+// const verseFile = new File(path.resolve(__dirname, `../config/verses-${moment.tz(new Date(), "Asia/Manila").isoWeek()}.toml`), {encoding: "utf-8"});
+const verseFile = new File(path.resolve(__dirname, `../config/verses.toml`), {encoding: "utf-8"});
 
-const dataFile = verseFile.readFileSync();
-const data = TOML.parse(dataFile);
+const verses = TOML.parse(verseFile.readFileSync());
 let tripped = false;
 
 module.exports = {
@@ -23,14 +23,15 @@ module.exports = {
             const date = moment.tz(new Date(), "Asia/Manila");
 
             if (date.hour() === 8 && !tripped) {
-                for (const key in data) {
-                    if (Object.hasOwnProperty.call(data, key)) {
-                        if (date.isoWeekday() === parseInt(key)) {
-                            const element = data[key];
+                for (const weekDay in verses) {
+                    if (Object.hasOwnProperty.call(verses, weekDay)) {
+                        if (date.isoWeekday() === parseInt(weekDay)) {
+                            const verse = verses[weekDay];
                             const embed = new Discord.MessageEmbed()
-                                .setTitle(element.location)
-                                .setDescription(element.verse)
+                                .setTitle(verse.location)
+                                .setDescription(verse.verse)
                                 .setColor("#0000ff");
+
 
                             webhook.send({embeds: [embed]});
                             break;
